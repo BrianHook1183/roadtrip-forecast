8a5948adbbebfa4b37ce0c8dd6b28a33c9e18e0c
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-08-07 13:33:13 -0500

    location objects now aggregate into itinerary array

--- main.js
+++ main.js
@@ -1,46 +1,94 @@
+// OpenCgeData api key
+const apiKeyCage = 'a4e6cc64bbe749ca8ec7aed6282a3091';
 // ClimaCell api key
 const apiKeyClima = 'MmdzZmqejYEWZI7bKBEA2KET3QwqKJJr';
 
-
-// OpenCgeData api key
-const apiKeyCage = 'a4e6cc64bbe749ca8ec7aed6282a3091';
-//  openCagedata URL variables
-let cageCity;
-let cageState;
+// Global Variables
+let itinerary = [];
 
 
 
 
 
-function handleForwardGeocoding() {
-  // console.log('handleForwardGeocoding ran');
+
+function handleForm() {
   $('#js-submit').click(e => {
     e.preventDefault();
-    // console.log('submit was clicked');
+    // console.log('addToItinerary was clicked');
     const cageCity = $('#js-city').val();
     const cageCityEncoded = encodeURIComponent(cageCity);
     const cageState = $('#js-state').val();
     const cageStateEncoded = encodeURIComponent(cageState);
-    console.log('city is ' + cageCity + ' and state is ' + cageState)
-    // test OpenCageData endpoint
-    const cageTestUrl = `https://api.opencagedata.com/geocode/v1/json?key=${apiKeyCage}&no_annotations=1&limit=1&q=${cageCityEncoded}%2C%20${cageStateEncoded}&countrycode=us`;
-    console.log('the fetched url will be: ' + cageTestUrl);
-    fetch(cageTestUrl)
-    .then(response => response.json())
-    .then(responseJson => setCoordinates(responseJson));
+    const startDate = $('#js-date').val();
+    console.log('city is ' + cageCity + ' and state is ' + cageState + ' and the arrival date is ' + startDate);
+    
+    const locationObject = 
+    {
+      'itCity': cageCity,
+      'itState': cageState,
+      'itCityEnc': cageCityEncoded,
+      'itStateEnc': cageStateEncoded,
+      'itDate': startDate
+    };
+    itinerary.push(locationObject);
+    console.log('itinerary before geocoding is:');
+    console.log(itinerary);
+
+    clearForm();
+    handleForwardGeocoding(cageCityEncoded, cageStateEncoded);
   })
 }
 
+function clearForm() {
+  $('#js-city').val('');
+  $('#js-state').val('');
+  $('#js-date').val('');
+}
+
+function handleForwardGeocoding(cageCityEncoded, cageStateEncoded) {
+  console.log('handleForwardGeocoding ran');
+  // test OpenCageData endpoint
+  const cageTestUrl = `https://api.opencagedata.com/geocode/v1/json?key=${apiKeyCage}&no_annotations=1&limit=1&q=${cageCityEncoded}%2C%20${cageStateEncoded}&countrycode=us`;
+  console.log('the fetched url will be: ' + cageTestUrl);
+  fetch(cageTestUrl)
+  .then(response => response.json())
+  .then(responseJson => setCoordinates(responseJson));
+}
 function setCoordinates(responseJson) {
   // console.log(responseJson);
   //  limit=1 in endpoint ensures that [0] is the only "results" to access
   const cageDescription = responseJson.results[0].formatted;
   const cageLat = responseJson.results[0].geometry.lat;
   const cageLng = responseJson.results[0].geometry.lng;
-  // console.log(cageLat + ', ' + cageLng);
-  $('#js-geocoding-output').html('openCage API returned ' + cageDescription + ' with the coordinates ' + cageLat + ', ' + cageLng);
-  fetchForecast(cageLat, cageLng)
+    console.log(cageDescription + ' coordinates are: ' + cageLat + ', ' + cageLng);
+    console.log('the itinerary array from inside setCoordinates is: ');
+  // push geocoded coordinates into location object
+  //  the variable z is only temporary until i put this all into a loop
+  const z = itinerary.length-1;
+  itinerary[z].itLat= cageLat;
+  itinerary[z].itLng= cageLng;
+    console.log(itinerary);
+
+  // Temporarily gives feedback on adding item to itinerary to give feedback that your location is correct
+  $('#js-geocoding-output').html('Make sure your intended location was: ' + cageDescription);
+  // fetchForecast(cageLat, cageLng)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function fetchForecast(cageLat, cageLng){
   // console.log('fetchForecast was initialized');
@@ -75,4 +123,4 @@ function displayForecast(responseJson) {
 }
 
 
-$(handleForwardGeocoding);
+$(handleForm);
