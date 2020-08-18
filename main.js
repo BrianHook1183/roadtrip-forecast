b8f38bc5396aa16594340bc64767d8b9c95acc4c
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-08-18 16:08:41 -0500

    removed all unused code and unnecessary comments from html js and css for submission

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 // ****Global Variables*****
-
-// ClimaCell's API is inconsistent with returning the requested date range, these variables will adjust the user inputted dates in the background for the fetch.
-// startDate can not be adjusted more than endDate or else for a 1 day forecast you will have an invalid call to the API. checkAdjustments(); will fix this automatically
-// let adjustStartDate = 0;
-// let adjustEndDate = 0;
 // OpenCgeData api key
 const apiKeyCage = 'a4e6cc64bbe749ca8ec7aed6282a3091';
 // ClimaCell api key
@@ -142,11 +137,8 @@ Date.prototype.toDateInputValue = (function() {
 let today14 = new Date().addDays(14).toDateInputValue();
 let today = new Date().toDateInputValue();
 
-
-
 function handleStart() {
   $('#js-start').click( e => {
-    // navigation button
     $('.start').slideUp();
     insertDateLimits();
     $('.setup').removeClass('hide');
@@ -159,7 +151,6 @@ function insertDateLimits() {
   // sets the datepicker display for minimum date for departure to be >= than arrival date. 
   $('#js-dep-date').click( e => {
     const  activeArrivalDate = $('#js-arr-date').val();
-    // console.log('js-dep-date clicked and the activeArrivalDate is: ' + activeArrivalDate);
     $('#js-dep-date').attr('min', activeArrivalDate);
   });
 }
@@ -172,19 +163,13 @@ function handleForm() {
     e.preventDefault();
     const cageCity = $('#js-city').val();
     const cageCityEncoded = encodeURIComponent(cageCity);
-    // checkAdjustments();
     const startDate = $('#js-arr-date').val();
-    // const startYear = startDate.slice(0, 4);
     const startMonth = startDate.slice(5, 7);
     const startDay = startDate.slice(8, 10);
-    // const startDateAdj = new Date(startYear, startMonth-1, startDay).addDays(adjustStartDate).toDateInputValue();
     const endDate = $('#js-dep-date').val();
-    // const endYear = endDate.slice(0, 4);
     const endMonth = endDate.slice(5, 7);
     const endDay = endDate.slice(8, 10);
-    // const endDateAdj = new Date(endYear, endMonth-1, endDay).addDays(adjustEndDate).toDateInputValue();
-    // console.log('startDate: ' + startDate + ' adjusted: ' + startDateAdj + ' endDate: ' + endDate + ' adjusted: ' + endDateAdj);
-    // Prevents from going any further if a required form input is missing
+    // Prevents from going any further if a required form input is missing (required attribut doesn't work on html form)
     if (!cageCity || !startDate || !endDate){
       alert("Missing required field!!");
       return;
@@ -196,9 +181,7 @@ function handleForm() {
       'itCity': cageCity,
       'itCityEnc': cageCityEncoded,
       'itStartDate': startDate,
-      // 'itStartDateAdj': startDateAdj,
       'itEndDate': endDate,
-      // 'itEndDateAdj': endDateAdj,
       'itStartMonth': startMonth,
       'itStartDay': startDay,
       'itEndMonth': endMonth,
@@ -208,24 +191,8 @@ function handleForm() {
   })
 }
 
-// removes possiblity tha the start date will be moved ahead of the end date
-// function checkAdjustments() {
-//   console.log(adjustEndDate);
-//   if (adjustStartDate > adjustEndDate) {
-//     adjustEndDate = adjustStartDate;
-//     console.log(adjustEndDate);
-//   };
-// }
-
-function clearForm() {
-  $('#js-city').val('');
-  $('#js-arr-date').val('');
-  $('#js-dep-date').val('');
-}
-
 function forwardGeocoding(locationObject) {
   const cageUrl = `https://api.opencagedata.com/geocode/v1/json?key=${apiKeyCage}&no_annotations=1&limit=1&q=${locationObject.itCityEnc}&countrycode=us`;
-  // console.log('the geocoding fetched url will be: ' + cageUrl);
   fetch(cageUrl)
   .then(response => {
     if (response.ok) {
@@ -235,7 +202,6 @@ function forwardGeocoding(locationObject) {
   })
   .then(responseJson => setCoordinates(responseJson, locationObject))
   .catch(err => {
-    // $('#js-error-message').text(`Something went wrong: ${err.message}`);
     alert(`Invalid location: check spelling`);
     // hides loading graphic while user fixes location input
     $('#js-loading1').addClass('hide');
@@ -243,16 +209,14 @@ function forwardGeocoding(locationObject) {
 }
 
 function setCoordinates(responseJson, locationObject) {
-  //  limit=1 in endpoint ensures that [0] is the only "results" to access
+  //  limit=1 in endpoint url ensures that [0] is the only "results" to access
   const cageDescription = responseJson.results[0].formatted;
   const cageLat = responseJson.results[0].geometry.lat;
   const cageLng = responseJson.results[0].geometry.lng;
-  // console.log(cageDescription + ' coordinates are: ' + cageLat + ', ' + cageLng);
   // removes ", United States of America" from each returned location
   locationObject.itDesc= cageDescription.split(",", 2);
   locationObject.itLat= cageLat;
   locationObject.itLng= cageLng;
-  // push final form of locationObject into the global itinerary
   itinerary.push(locationObject);
   displayItinerary();
 }
@@ -267,45 +231,44 @@ function displayItinerary() {
   itinerary.forEach((city, z) => {
     $('.js-itinerary').append(`
     <div class="itinerary-object">
-      <div class="stop-header">
-        <button class="js-delete x" id="${z}">X</button>
-        <h4>${city.itDesc}</h4>
+    <div class="stop-header">
+    <button class="js-delete x" id="${z}">X</button>
+    <h4>${city.itDesc}</h4>
       </div>
       <div class="stop-duration">
         <span><strong>${city.itStartMonth}/${city.itStartDay}</strong>:Arrive -</span><span>- Depart: <strong> ${city.itEndMonth}/${city.itEndDay}</strong></span>
-      </div>
-    </div>
-    `);
-  });
-  // Hide loading graphic after itinerary has displayed
-  $('#js-loading1').addClass('hide');
-  // unblue the Itinerary Heading
-  $('.js-intinerary-title').removeClass('blur');
-  // reveal forecast buton
-  $('.js-fetch').removeClass('hide');
-  clearForm();
+        </div>
+        </div>
+        `);
+      });
+      $('#js-loading1').addClass('hide');
+      $('.js-intinerary-title').removeClass('blur');
+      $('.js-fetch').removeClass('hide');
+      clearForm();
+    }
+    
+function clearForm() {
+  $('#js-city').val('');
+  $('#js-arr-date').val('');
+  $('#js-dep-date').val('');
 }
-
+    
 function resetItinerary() {
   itinerary = [];
   displayItinerary();
   $('.js-fetch').addClass('hide');
   $('.js-intinerary-title').addClass('blur');
 }
-
+  
 function handleItinerary() {
   $('.js-itinerary').on('click', '.js-delete' , e => {
-    console.log(itinerary);
     deleteItineraryItem(e.target.id);
   })
 }
 
 function deleteItineraryItem(buttonId) {
-  console.log('deleteItineraryItem was clicked for button id: ' + buttonId);
-  // TO DO: figure out a way to bind each delete button with the index value that it holds in the itinerary array
   itinerary.splice(buttonId, 1);
   displayItinerary();
-  console.log(itinerary);
 }
 
 function handleForecasts() {
@@ -320,28 +283,21 @@ function handleForecasts() {
     populateForecastStop([...itinerary]);
   })
   $('#js-back').click(e => {
-    // navigation button
     $('.setup').slideDown();
     $('.forecast').addClass('hide');
   })
 }
 
-// Recursively populating forecast. (instead of  itinerary.forEach(fetchForecast);)
 function populateForecastStop(itinerary) {
-  // base case
   if (itinerary.length === 0){
     return;
   }
-  // .shift() is acting as a -- incrementer, if this were a loop, the base case above is the condition to stop running the loop.
-  // Temporarily removed &start_time=${item.itStartDateAdj}&end_time=${item.itEndDateAdj} from fetch because API is hisbehaving.
   item = itinerary.shift();
   const climaUrl = `https://api.climacell.co/v3/weather/forecast/daily?apikey=${apiKeyClima}&lat=${item.itLat}&lon=${item.itLng}&start_time=now&unit_system=us&fields=precipitation,feels_like,precipitation_probability,weather_code`;
-    // console.log('the url to be fetched is: ' + climaUrl);
   fetch(climaUrl)
   .then(response => response.json())
   .then(responseJson => {
     displayForecast(responseJson, item.itDesc, item.itStartDate, item.itEndDate);
-    // calls itself as part of recursion
     populateForecastStop(itinerary);
   });
 }
@@ -351,7 +307,7 @@ function displayForecast(responseJson, itDesc, itStartDate, itEndDate) {
     if (!responseJson[i]){
       continue;
     }
-    // Filters out the unwanted days that are supplied by forecast API either inherently or from adjustment on user date range
+    // Filters out the unwanted days that are supplied by forecast API
     if (responseJson[i].observation_time.value >= itStartDate && responseJson[i].observation_time.value <= itEndDate){
       const weatherCode = responseJson[i].weather_code.value;
       // default for if there is no code match
@@ -365,11 +321,11 @@ function displayForecast(responseJson, itDesc, itStartDate, itEndDate) {
           weatherObject = item;
         };
       };
-      // overwrites default weatherObject if code match is found
+      // overwrites default weatherObject if code match was found
       const weatherImage = weatherObject.codeImage;
       const weatherText = weatherObject.codePretty;
       const weatherImageAlt = weatherObject.codePretty;
-      // adds completed forecast for each day into the displayed forecast
+      // adds completed forecast for each day into the DOM
       $('.js-results').append('<div class="forecast-pair"><div class="forecast-text"><ul><li>Overview: <strong>' + weatherText + '</strong></li><li>' + responseJson[i].precipitation_probability.value +  responseJson[i].precipitation_probability.units + ' chance of precipitation</li><li>"Feels Like" temperature:</li><ul><li>min: ' + responseJson[i].feels_like[0].min.value + ' &#8457;</li><li>max: ' + responseJson[i].feels_like[1].max.value + ' &#8457;</li></ul></ul></div><div class="forecast-picture"><img src="assets/' + weatherImage + '" alt="' + weatherImageAlt + '"><div class="hover-text"><h3>' + itDesc + '<br>' + responseJson[i].observation_time.value.slice(5, 10) + '</h3></div></div></div><hr>');
     };
   }
